cea17982115f7ca7c6069a67e40afae64229e559
fricktofarah <frickto.farah@gmail.com>
2026-01-19 15:23:41 +0100

    Remove app-level www redirect to avoid loop

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.drinkvelah.com" }],
-        destination: "https://drinkvelah.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
   // you can add other options here later
 };
 
